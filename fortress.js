203404117abb4ed320a22852bb90dcb8b2b82a91
Noah Kim <noahbkim@gmail.com>
2016-05-19 15:01:40 -0400

    Some demo drawing code.

--- fortress.js
+++ fortress.js
@@ -1,5 +1,25 @@
-
-function Fortress(canvas){
+function Fortress(canvas) {
+    
+    /* Super constructor and superclass reference. */
 	Engine.call(this, canvas);
     var superclass = new Engine();
+    
+    /* Render. */
+    this.render = function(delta) {
+        
+        /* Clear. */
+        this.context.fillStyle = "white";
+        this.context.fillRect(0, 0, 800, 600);
+        
+        /* Demo, draw the title. */
+        this.context.fillStyle = "black";
+        this.context.font = "36px Franklin Gothic Medium";
+        this.context.textAlign = "center";
+        this.context.fillText("Bill Fortress", 400, 200);
+        this.context.font = "24px Franklin Gothic Medium";
+        this.context.fillText("A game of legislation", 400, 240);
+        this.context.font = "20px Franklin Gothic Medium";
+        this.context.fillText("Click to begin", 400, 420); 
+    }
+
 }
