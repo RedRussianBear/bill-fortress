d1de39587ead2b3a97f6832e7e2ac7d7952f73a4
Noah Kim <noahbkim@gmail.com>
2016-05-22 15:53:06 -0400

    Working on the GUI code.

--- gui.js
+++ gui.js
@@ -1,91 +1,94 @@
+/** Graphical interface sprites. */
+var gui = {};
 
-/* GUI Component class, used to house all other elements */
-function Component(x, y, w, h, engine, gui, style) {
-	//initialize fields
-	this.x = x;
-	this.y = y;
-	this.width = w;
-	this.height = h;
-	this.style = style;
-	this.engine = engine;
-	this.gui = gui;
-	this.state = Component.ACTIVE;
-	
-	this.children = [];
-	
-	/* Render function, draws self and all child components */
-	this.render = function(context) {
-		this.engine.context.fillStyle = this.style;
-		this.engine.context.fillRect(this.x, this.y, this.width, this.height);
-		
-		for(var i = 0; i < this.children.length; i++)
-			this.children[i].render(context);
-	}
-	
-	this.addChild = function(child) {
-		this.children.push(child);
-	}
-	
-	this.update = function() {
-		for(var i = 0; i < this.children.length; i++) {
-			var current = this.children[i];
-			
-			if(current.update)
-				current.update();
-		}
-	}
+/** The basic interface component. */
+gui.Component = function Component(engine, parent, x, y, w, h, style) {
+    
+    /** Instantiate sprite. */
+    sprite.Sprite.call(this, x, y, w, h);
+    
+    /** Retain other members. */
+    this.engine = engine;
+    this.parent = parent;
+    this.style = style;
+    
+    /** Instance. */
+    this.visible = true;
+    this.state = Component.NORMAL;
+    this.children = {};
+    
+    /** Add a child to the component. */
+    this.adopt = function(name, component) { this.children[name] = component; }
+    
+    /** Remove a child from the component. */
+    this.remove = function(name) { name in this.children && delete this.children[name]; }
+    
+    /** Render the component. */
+    this.render = function(context) {
+        if (!this.visible) return;
+        for (var name in this.children) this.children[name].render(context);
+    }
+    
+    /** Update the component. */
+    this.update = function(delta) {
+        if (this.state == Component.DISABLED) return;
+        for (var name in this.children) this.children[name].update(delta);
+    }
+
 }
 
-/* Component state constants */
-Component.HIDDEN = 0;
-Component.ACTIVE = 1;
+/** Component relative transform and states. */
+gui.Component.prototype = {
+    get relative() { return this.parent.relative && this.transform.with(this.parent.relative) || this.transform; },
+    DISABLED: 0, NORMAL: 1
+}
 
-function Button(x, y, w, h, parent, tex, onPress, isUnlocked) {
-	/* Basic Variables */
-	this.parent = parent;
-	this.width = w;
-	this.height = h;
-	this.x = x;
-	this.y = y;
-	this.text = tex;
+/** Button component class. */
+gui.Button = function Button(parent, x, y, w, h, text, mousedown) {
+    
+	/* Component superclass. */
+	gui.Component.call(this, engine, parent, x, y, w, h);
+
+    /** Specific to the button. */
+    this.text = text;
+    this.mousedown = typeof mousedown == "function" && mousedown || function() {};
 
 	/* States and functions */
-	this.state = Button.CLEAR;
-	this.onPress = onPress;
-	this.isUnlocked = isUnlocked;
+	this.state = Button.NORMAL;
+	this.mousedown = mousedown;
 
 	/* Draw Styling */
-	this.styles = [];
-	this.styles[Button.CLEAR] = {BOX: "white", BORDER: "gray", TEXT: "black"};
-	this.styles[Button.MOUSE_OVER] = {BOX: "gray", BORDER: "black", TEXT: "black"};
-	this.styles[Button.LOCKED] = {BOX: "gray", BORDER: "black", TEXT: "black"};
+    this.style = {font: "20px Franklin Gothic Medium", textAlign: "center"};
+	this.styles = {};
+	this.styles[Button.DISABLED] = {box: {fillStyle: "gray", strokeStyle: "black"}, text: {fillStyle: "black"}};
+	this.styles[Button.NORMAL] = {box: {fillStyle: "white", strokeStyle: "gray"}, text: {fillStyle: "black"}};
+	this.styles[Button.HOVER] = {box: {fillStyle: "gray", strokeStyle: "black"}, text: {fillStyle: "black"}};
 	
-	/* Text format variables */
-	this.font = "20px Franklin Gothic Medium";
-	this.fontHeight = 20;
-	this.align = "left";
-	this.textOffTop = this.height/2 - this.fontHeight/2;
-	this.textOffLeft = 3;
-
-	/* Render button */
+	/** Render button the button. */
 	this.render = function(context) {
-		context.fillStyle = this.styles[this.state].BOX;
-		context.fillRect(this.x + this.parent.x, this.y + this.parent.y, this.width, this.height);
-		context.strokeStyle = this.styles[this.state].BORDER;
-		context.strokeRect(this.x + this.parent.x, this.y + this.parent.y, this.width, this.height);
-	
-		context.font = this.font;
-		context.textAlign = this.align;
-		context.fillStyle = this.styles[this.state].TEXT;
+
+        /* Transform relative to parent. */
+        var transform = this.relative;
+        
+        /* Save the state of the context. */
+        context.save();
+        
+        /* Draw the box. */
+        for (var key in this.styles.box) this.context[key] = this.styles.box[name];
+		context.fillRect(transform.x, transform.y, this.width, this.height);
+		context.strokeRect(transform.x, transform.y, this.width, this.height);
+        
+        /* Draw the text. */
+        for (var key in this.styles.text) this.context[key] = this.styles.text[name];
 		context.fillText(this.text, this.x + this.parent.x + this.textOffLeft, this.y + this.parent.y + this.textOffTop);
+        
+        /* Restore the context. */
+        context.restore();
+        
 	}
 	
-	this.update = function() {
-		if(this.isUnlocked())
-			this.state = Button.CLEAR;
-		else
-			this.state = Button.LOCKED;
-		
+    /** Update the button. */
+	this.update = function(delta) {
 		if(this.parent.engine.input.mouse.x > this.x && this.parent.engine.input.mouse.x < this.x + this.width &&
 		this.parent.engine.input.mouse.y > this.y && this.parent.engine.input.mouse.y < this.y + this.height 
 		&& this.state != Button.LOCKED) {
@@ -97,9 +100,7 @@ function Button(x, y, w, h, parent, tex, onPress, isUnlocked) {
 }
 
 /* Button state constants */
-Button.CLEAR = 0;
-Button.MOUSE_OVER = 1;
-Button.LOCKED = 2;
+gui.Button.prototype.HOVER = 2;
 
 function Text(x, y, parent, tex, style, font, align) {
 	this.align = align;
