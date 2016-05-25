98c2d5a732e42860aa2685ebd9ccd76265fe516e
RedRussianBear <mkhrenov34@gmail.com>
2016-05-24 21:22:24 -0400

    Player movement

--- bill.js
+++ bill.js
@@ -8,8 +8,58 @@ function Bill(engine, name, type) {
 	
 	this.render = function(context) {	}
 	
-	this.update = function() {
+	this.update = function(delta) {
+		this.state = Bill.PASSIVE;
+		var dist = Bill.SPEED * (delta/this.engine._update.interval);
+		for(key in this.engine.input.keyboard) {
+			switch(key) {
+				case "W":
+					this.state = Bill.UP;
+					if(this.canMove(Bill.UP, dist))
+						this.transform.y -= dist;
+					break;
+				case "A":
+					this.state = Bill.LEFT;
+					if(this.canMove(Bill.LEFT, dist))
+						this.transform.x -= dist;
+					break;
+				case "S":
+					this.state = Bill.DOWN;
+					if(this.canMove(Bill.DOWN, dist))
+						this.transform.y += dist;
+					break;
+				case "D":
+					this.state = Bill.RIGHT;
+					if(this.canMove(Bill.RIGHT, dist))
+						this.transform.x += dist;
+					break;
+			}
+		}
+	}
+	
+	this.canMove = function(direction, dist) {
+		var grid = this.engine.getWorld().grid;
 		
+		switch(direction){
+			case Bill.UP:
+				if(grid[Math.floor(this.transform.x/world.BOXSIZE)][Math.floor((this.transform.y - dist)/world.BOXSIZE)] != 0 || grid[Math.floor((this.transform.x + this.width)/world.BOXSIZE)][Math.floor((this.transform.y - dist)/world.BOXSIZE)])
+					return false;
+				break;
+			case Bill.DOWN:
+				if(grid[Math.floor(this.transform.x/world.BOXSIZE)][Math.floor((this.transform.y + dist)/world.BOXSIZE)] != 0 || grid[Math.floor((this.transform.x + this.width)/world.BOXSIZE)][Math.floor((this.transform.y + dist)/world.BOXSIZE)])
+					return false;
+				break;
+			case Bill.EAST:
+				if(grid[Math.floor((this.transform.x - dist)/world.BOXSIZE)][Math.floor((this.transform.y)/world.BOXSIZE)] != 0 || grid[Math.floor((this.transform.x - dist)/world.BOXSIZE)][Math.floor((this.transform.y + this.height)/world.BOXSIZE)])
+					return false;
+				break;
+			case Bill.WEST:
+				if(grid[Math.floor((this.transform.x + dist)/world.BOXSIZE)][Math.floor((this.transform.y)/world.BOXSIZE)] != 0 || grid[Math.floor((this.transform.x + dist)/world.BOXSIZE)][Math.floor((this.transform.y + this.height)/world.BOXSIZE)])
+					return false;
+				break;
+		}
+		
+		return true;
 	}
 }
 
@@ -24,3 +74,5 @@ Bill.HEIGHT = 96;
 
 Bill.XPOS = 0;
 Bill.YPOS = 0;
+
+Bill.SPEED = 5;
